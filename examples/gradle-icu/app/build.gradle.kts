aaaab7f4d9c1884409c60b4b91fd1168bfca9593
Jean Aurambault <jeanaurambault@pinterest.com>
2022-07-19 20:42:56 -0700

    test: Fix Gradle ICU tests

--- examples/gradle-icu/app/build.gradle.kts
+++ examples/gradle-icu/app/build.gradle.kts
@@ -8,14 +8,25 @@ repositories {
 }
 
 dependencies {
+    // l10nmessages
     implementation("com.pinterest.l10nmessages:l10nmessages:0.0.9-SNAPSHOT")
     implementation("com.ibm.icu:icu4j:70.1")
     annotationProcessor("com.pinterest.l10nmessages:l10nmessages-proc:0.0.9-SNAPSHOT")
     annotationProcessor("com.ibm.icu:icu4j:70.1")
     annotationProcessor(files("src/main/resources"))
-}
 
+    // for tests - not needed by l10nmessages
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
 
 application {
     mainClass.set("com.pinterest.l10nmessages.example.Application")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
