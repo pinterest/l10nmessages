70318069c93eea0216c113027c2d77e1a7ac85fb
Jean Aurambault <jeanaurambault@pinterest.com>
2022-09-20 15:11:22 -0700

    release: prepare next version 1.0.2-SNAPSHOT

--- examples/gradle/app/build.gradle.kts
+++ examples/gradle/app/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 
 dependencies {
     // l10nmessages
-    implementation("com.pinterest.l10nmessages:l10nmessages:1.0.1")
-    annotationProcessor("com.pinterest.l10nmessages:l10nmessages-proc:1.0.1")
+    implementation("com.pinterest.l10nmessages:l10nmessages:1.0.2-SNAPSHOT")
+    annotationProcessor("com.pinterest.l10nmessages:l10nmessages-proc:1.0.2-SNAPSHOT")
     annotationProcessor(files("src/main/resources"))
 
     // for tests - not needed by l10nmessages
